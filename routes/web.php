ba76f326b441f65b39bfb5bbd8a0a12497b61089
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-04-13 11:46:15 -0500

    sa

--- routes/web.php
+++ routes/web.php
@@ -22,36 +22,20 @@ Route::get('/tickets', [App\Http\Controllers\HomeController::class, 'tickets'])-
 Route::get('/ganadores', [App\Http\Controllers\HomeController::class, 'ganadores'])->name('ganadores');
 
 
-Route::get('/fix-server', function () {
-
-    // 🔐 Clave de seguridad (cámbiala sí o sí)
-    if (request('key') !== '123456789asdfghjkl') {
-        abort(403, 'No autorizado');
-    }
-
-    $output = [];
+Route::get('/fix-config', function () {
 
     try {
-        Artisan::call('optimize:clear');
-        $output[] = 'optimize:clear OK';
-
         Artisan::call('config:clear');
-        $output[] = 'config:clear OK';
-
         Artisan::call('cache:clear');
-        $output[] = 'cache:clear OK';
-
         Artisan::call('route:clear');
-        $output[] = 'route:clear OK';
-
         Artisan::call('view:clear');
-        $output[] = 'view:clear OK';
+        Artisan::call('optimize:clear');
 
+        return '✔ Limpieza completada correctamente.';
     } catch (\Exception $e) {
-        return 'Error: ' . $e->getMessage();
+        return '❌ Error: ' . $e->getMessage();
     }
 
-    return implode('<br>', $output);
 });
 
 
