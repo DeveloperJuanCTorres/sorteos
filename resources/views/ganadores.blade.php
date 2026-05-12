4e6d98b946b291e78f62856388b3d8d221e5edf2
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-05-12 16:26:57 -0500

    ca

--- resources/views/ganadores.blade.php
+++ resources/views/ganadores.blade.php
@@ -85,7 +85,7 @@
                     <div class="flex flex-wrap gap-2">
                         @foreach($winner->tickets as $ticket)
                             <span class="px-2 py-1 text-xs bg-primary/20 text-primary rounded-lg">
-                                {{ $ticket->dni }} - {{ $ticket->nombres . ' ' . $ticket->apellidos }} - {{ $ticket->departamento }}
+                                {{ $ticket->id }} - {{ $ticket->nombres . ' ' . $ticket->apellidos }} - {{ $ticket->departamento }}
                             </span>
                         @endforeach
                     </div>
